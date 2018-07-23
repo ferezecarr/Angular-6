4e8d69259db4ca773dadf1ef7035193c45eea7e6
ferezecarr <ferezecarr@gmail.com>
2018-07-23 15:36:11 -0300

    Proyecto con Angular 6

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  name: string;
+  age: number;
+  users: string[] = ['Juan','Pepe','Pedro'];
+  posts = [];
+
+  constructor(private dataService : DataService) {
+    this.dataService.getData().subscribe(data => {
+      this.posts = data;
+    });
+  }
+
+  addUser(newUser) {
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+
+  deleteUser(user) {
+    for(let i = 0 ; i < this.users.length ; i++) {
+      if(user == this.users[i]) {
+        this.users.splice(i , 1);
+      }
+    }
+  }
+
+
+
+}
